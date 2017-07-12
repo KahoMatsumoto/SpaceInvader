6821d1bba194dca633a2d3d125797542a72de30a
Kaho Matsumoto(まつも) <k.95038chuckle+git@gmail.com>
2017-07-12 16:19:57 +0900

    スコアUIの追加

---
+++ SpaceInvader/Assets/script/UIDirector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class UIDirector : MonoBehaviour {
+
+    public Text scoreText; // Text用変数
+    private int score = 0; // スコア計算用変数
+
+
+	// Use this for initialization
+	void Start () {
+        scoreText.text = "Score:0000"; //初期スコアを代入して画面に表示
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
