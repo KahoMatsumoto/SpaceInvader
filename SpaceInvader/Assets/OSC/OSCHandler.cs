4f5c2c426fbb443eb3e663dfc1850799c6c03a1b
kmats <matsumoto_k@aid-dcc.com>
2017-07-24 19:14:50 +0900

    フォルダ整理

--- SpaceInvader/Assets/OSC/OSCHandler.cs
+++ SpaceInvader/Assets/OSC/OSCHandler.cs
@@ -1,23 +1,3 @@
-//
-//	  UnityOSC - Open Sound Control interface for the Unity3d game engine	  
-//
-//	  Copyright (c) 2012 Jorge Garcia Martin
-//
-// 	  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
-// 	  documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-// 	  the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-// 	  and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-// 
-// 	  The above copyright notice and this permission notice shall be included in all copies or substantial portions 
-// 	  of the Software.
-//
-// 	  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
-// 	  TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
-// 	  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-// 	  CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// 	  IN THE SOFTWARE.
-//
-//	  Inspired by http://www.unifycommunity.com/wiki/index.php?title=AManagerClass
 
 using System;
 using System.Net;
@@ -98,7 +78,7 @@ public class OSCHandler : MonoBehaviour
         //Initialize OSC servers (listeners)
         //Example:
 
-        CreateServer("Unity", 55555);
+        CreateServer("Unity", 55055);
 	}
 	
 	#region Properties
