3c4d8a18c7f5047706ce73c14432d69285a19ee0
KahoMatsumoto <k.95038chuckle@gmail.com>
2017-08-21 22:36:03 +0900

    ufoできた

---
+++ SpaceInvader/Assets/scripts/UFO/UFOController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UFOController : MonoBehaviour {
+
+	GameObject bullet;
+	public int value;
+
+	// Use this for initialization
+	void Start () {
+		this.bullet = GameObject.Find ("bullet");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject fish = GameObject.Find ("fish3_yellow10");
+		if (fish != null) {
+			Destroy (fish);
+		}
+
+		// 当たり判定
+		Vector2 p1 = transform.position; 				// UFOの中心座標
+		Vector2 p2 = bullet.transform.position; 	// 弾の中心座標
+		Vector2 dir2 = p1 - p2;
+		float d = dir2.magnitude;
+		float r1 = 1.0f;	// UFOの半径
+		float r2 = 0.1f;	// 弾の半径
+
+		if (d < r1 + r2) {
+			bullet.transform.position = 
+				GameObject.Find("ankou").transform.position;
+			UIDirector.AddScore (value);
+
+			StartCoroutine ("blink");
+		}
+	}
+	private IEnumerator blink(){
+
+		for (int i = 0; i < 2; i++) {
+			this.GetComponent<SpriteRenderer> ().color *= -1;
+
+			yield return new WaitForSeconds (0.1f);
+
+		}
+
+	}
+}
